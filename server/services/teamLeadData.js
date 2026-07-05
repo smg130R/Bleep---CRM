937c36ef209a5a378ddbc5054da0e5177310b943
smg130R <smg130R@users.noreply.github.com>
2026-07-05 12:54:19 +0530

    Fix batchDate column not found error

--- server/services/teamLeadData.js
+++ server/services/teamLeadData.js
@@ -45,7 +45,6 @@ async function addLeads(teamId, newLeads) {
         naCount: 0,
         assignedInMaster: false,
         currentAssigneeId: null,
-        batchDate: today,
         createdAt: today,
         updatedAt: today,
       });
